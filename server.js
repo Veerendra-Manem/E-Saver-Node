584934b7fc726ec2fc0b0e8ab2b6a8f008481941
Veerendra Kumar M <veerendra.manem@gmail.com>
2018-10-25 01:46:10 +0800

    Added Pledge API and make to calculate user score

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var jwt = require('jwt-simple')
 
 var User = require('./models/User.js')
 var Post = require('./models/Post.js')
+var Pledge = require('./models/Pledge.js')
 var auth = require('./auth.js')
 
 mongoose.Promise = Promise
@@ -22,7 +23,12 @@ app.get('/posts/:id', async (req, res) => {
 
 app.get('/profile', auth.checkAuthenticated, async (req, res) => {
     try {
+        var author = req.userId
+        var posts = await Post.find({ author })
         var user = await User.findById(req.userId, '-pwd -__v')
+        for(var i=0;i<posts.length;i++) {
+            user.score = (user.score === undefined ? 0 : user.score) + posts[i].score;
+        }
         res.send(user)
     } catch (error) {
         console.error(error)
@@ -43,9 +49,56 @@ app.get('/profile/todayposts', auth.checkAuthenticated, async (req, res) => {
     res.send(posts)
 })
 
+app.post('/createpledge', auth.checkAuthenticated, (req, res) => {
+    try {
+    var pledge = req.body
+    //console.log(pledge)
+    // var user = User.findById( req.userId, '-pwd -__v')       
+    // var userToSave = new User(user)
+    // userToSave.pledge = pledge.name
+    // console.log(userToSave)
+    // userToSave.save((err, result) => {
+    //     if (err) {
+    //         console.error('saving user pledge error')            
+    //     }
+    // })
+
+    var pledgePost = new Pledge(pledge)
+
+    pledgePost.save((err, result) => {
+        if (err) {
+            console.error('saving pledge error')            
+            return res.status(500).send({ message: 'saving pledge error' })
+        }
+
+        return res.sendStatus(200)
+    })
+} catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+}
+})
+
 app.post('/post', auth.checkAuthenticated, (req, res) => {
     var postData = req.body
     postData.author = req.userId
+
+    // var user = User.findById(req.userId, '-pwd -__v')
+    // var newscore = (user.score === undefined ? 0 : user.score) + postData.score;
+
+    // var user = new User(user)
+
+    // user.save((err, result) => {
+    //     if (err) {
+    //         console.error('saving user score error')            
+    //     }
+    // })
+
+    // db.user.findAndModify({
+    //     query: {"id": req.userId},
+    //     update: {"$set": {"score": newscore}},
+    //     new: true
+    //     })
 
     var post = new Post(postData)
 
@@ -61,18 +114,19 @@ app.post('/post', auth.checkAuthenticated, (req, res) => {
 
 app.get('/users', async (req, res) => {
     try {
-        var users = await User.find({}, '-pwd -__v')
-        // console.log(users)
-        // users.forEach(user => {
-        //     console.log(user)
-        //     var posts = Post.find({ user })
-        //     console.log(posts.count)
-        //     for(var i=0; i < posts.count; i ++) {
-        //         console.log(posts[i])
-        //         user.score = (user.score === undefined ? 0 : user.score) + posts[i].score;
-        //     }            
-        // })
-        res.send(users)
+        var posts = await Post.find({})
+        var users = await User.find({}, '-pwd -__v')        
+        for(var i=0;i<posts.length;i++) {
+            var post = posts[i]
+            
+            for(var u=0;u<users.length;u++) {
+                var user = users[u]
+                if(post.author == user.id) {
+                    user.score = (user.score === undefined ? 0 : user.score) + posts[i].score;
+                }                        
+            }
+        }
+        res.send(users)           
     } catch (error) {
         console.error(error)
         res.sendStatus(500)
@@ -96,4 +150,4 @@ mongoose.connect('mongodb://test:test123@ds033487.mlab.com:33487/esaver', {useNe
     )
 
 app.use('/auth', auth.router)
-app.listen(process.env.PORT || 3000)
+app.listen(3000)
